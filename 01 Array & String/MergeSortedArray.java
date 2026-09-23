fe0a74c1ae8e3dd87b226368c04493b2d4c54c3d
owokequ <xaeshkayt@gmail.com>
2026-09-24 00:14:33 +0300

    done MergeSortedArray

--- 01 Array & String/MergeSortedArray.java
+++ 01 Array & String/MergeSortedArray.java
@@ -1,8 +1,29 @@
 /**
  * LeetCode #88 — Merge Sorted Array
- * https://leetcode.com/problems/merge-sorted-array/
- *
- * TODO: Добавь сигнатуру метода с LeetCode и реализуй решение.
+ * 
+ * Идея: У нас два отсортированных массива, поэтому можем вставлять элементы с конца
+ *  
+ * 
  */
 public class MergeSortedArray {
+    public static void main(String[] args) {
+        merge(null, 0, null, 0);
+    }
+
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int one = m - 1; // последнее настоящие число в nums1
+        int two = n - 1; // последнее число в nums2
+        int three = m + n - 1; // последнея позиция результата
+
+        while (two >= 0) {
+            if(one >= 0 && nums1[one] > nums2[two]){
+                nums1[three] = nums1[one];
+                one--;
+            } else {
+                nums1[three] = nums2[two];
+                two--;
+            }
+            three--;
+        }
+    }
 }
