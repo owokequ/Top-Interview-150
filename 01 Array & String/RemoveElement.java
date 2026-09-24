4f05757ce445f16c2364991ddace163fdfebb4a5
owokequ <xaeshkayt@gmail.com>
2026-09-25 00:18:21 +0300

    RemoveElement done

--- 01 Array & String/RemoveElement.java
+++ 01 Array & String/RemoveElement.java
@@ -1,8 +1,34 @@
 /**
  * LeetCode #27 — Remove Element
- * https://leetcode.com/problems/remove-element/
  *
- * TODO: Добавь сигнатуру метода с LeetCode и реализуй решение.
  */
 public class RemoveElement {
+
+    public static void main(String[] args) {
+        int k = removeElement(new int[]{3, 2, 2, 3}, 3);
+        System.out.println(k);
+    }
+
+    public static  int removeElement(int[] nums, int val) {
+        int left = 0;
+        int right = nums.length - 1;
+        int buffer;
+        while(left <= right){
+            if(nums[left] == val && nums[right] != val){
+                buffer = nums[left];
+                nums[left] = nums[right];
+                nums[right] = buffer;
+                left++;
+                right--;
+            }else if(nums[left] == val && nums[right] == val){
+                right--;
+            } 
+            else{
+                left++;
+            }
+        }
+        
+        return right + 1;
+    }
+
 }
